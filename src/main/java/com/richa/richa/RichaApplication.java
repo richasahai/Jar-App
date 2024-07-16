1b8491b42ed65c6f34953ab6f918a2eaa7dda12a
Richa <sahai.ei@gmail.com>
2024-07-16 21:07:29 +0530

    Initial commit

---
+++ src/main/java/com/richa/richa/RichaApplication.java
@@ -0,0 +1,13 @@
+package com.richa.richa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RichaApplication.class, args);
+	}
+
+}
